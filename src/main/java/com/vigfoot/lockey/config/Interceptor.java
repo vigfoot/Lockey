0b9e898168c3b84b770ded2ebf939d47a2ff6826
HyeonSeok Ko <bigfoot7774@gmail.com>
2023-11-22 11:37:55 +0900

    initialize Lockey

---
+++ src/main/java/com/vigfoot/lockey/config/Interceptor.java
@@ -0,0 +1,6 @@
+package com.vigfoot.lockey.config;
+
+public class Interceptor {
+
+
+}
---
+++ src/main/java/com/vigfoot/lockey/service/ValidationService.java
@@ -0,0 +1,9 @@
+package com.vigfoot.lockey.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValidationService {
+
+
+}
